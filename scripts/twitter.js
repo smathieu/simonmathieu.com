86eda8d705d298df5f194fee3f11a93bfc838b2b
Simon Mathieu <simon@desktop.(none)>
2011-03-31 20:57:40 -0400

    Fix layout and styling. Mostly things related to twitter link.

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,7 +1,12 @@
 $(function() {
+  function sleep(milliSeconds){
+    var startTime = new Date().getTime(); // get the current time
+    while (new Date().getTime() < startTime + milliSeconds); // hog cpu
+  }
+ 
   var user_name = 'smathieu';
   var url = 'http://api.twitter.com/1/statuses/user_timeline.json'; 
-  var number_of_tweets = 3;
+  var number_of_tweets = 1;
 
   args = {
     count: number_of_tweets,
@@ -9,11 +14,12 @@ $(function() {
   };
   $.get(url, args, function(tweets){ 
     var $ul = $('#tweet_list');
+    $ul.find('li').remove();
     for (var i = 0; i < number_of_tweets; i++) {
       var tweet = tweets[i];
       if (tweet) {
         $li = $('<li>' + tweet.text + '</li>').linkify()
-        $ul.append($li)
+        $ul.hide().append($li).fadeIn()
       }
     }
   }, 'jsonp');
